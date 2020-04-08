fced70fde29996d3d551dac00a96c58c672bee40
omarsobhy11 <32989189+omarsobhy11@users.noreply.github.com>
2020-04-09 01:01:39 +0200

    second commit

--- resources/views/pages/About.blade.php
+++ resources/views/pages/About.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!doctype html> 
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
@@ -8,8 +8,63 @@
     <title>{{config('app.name', 'PizaaApp')}}</title>
 
     </head>
-    <body>
-        <h1>About</h1>
-        <p>this is the about page made by abdallah mohamed</p>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>{{config('app.name', 'PizaaApp')}}</title>
+<style>
+    h1 {
+        color:  #53352d;
+        font-family:Arial, Helvetica, sans-serif;
+        font-size: 600%;
+        text-shadow: 2px 2px 5px whitesmoke;
+        padding-top: 40px;
+        text-decoration-line: overline;
+        text-decoration-style: solid;
+        text-decoration-color: #FF5733;
+    }
+    p  {
+        margin-right: auto;
+        margin-left: auto;
+        width: 100%;
+        text-align: center;
+        color: #53352d;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 120%;
+        padding-top: 50px;
+        padding-bottom: 50px;	 
+        padding-left: 35px;     
+        padding-right: 35px;  
+     
+    }
+    span {
+
+        color:#FF5733 ;
+        font-size:102%;
+    }
+    </style>
+
+</head>
+<body>
+    <h1>About</h1>
+    
+</body>
+<body style="background-color:white;">
+    <h1 style="text-align:center;">About Krusty Krab</h1>
+    <p style="text-align:center;"> In the year 1947, a prominent name in the world of restaurants was born. In no time, he became the talk of the town; a restaurant on the bank of the Nile in Downtown Cairo which was known as The Castle of the Nile for it was located right next to the beautiful Nile River. This area has been renowned for being one of the most elegant and classy areas in Egypt due to its proximity to some of the world’s most famous hotels. An Oriental restaurant started serving a variety of oriental grills with a specialization in the Egyptian cuisine. Arabs and foreigners used to come all the time until the restaurant’s name started to expand and popularize until it became one of Egypt’s landmarks. <br><br>
+        It became one of Egypt’s landmarks just like the Nile, the Tower, and even the Giza Pyramids. It was the first restaurant to introduce the idea of tent celebrations and Oriental Nights for foreigners. These beautiful evenings were spent by the Pyramids and in the boats of the Nile. Inspired by the presence of Arabs, foreigners, and tourists in the area of Cairo’s Downtown, Garden City, and hotels by the Nile, <span><span>Krusty Krab</span></span> was the first to introduce the concept of home delivery by the motorbike through which Egyptian employees delivered the food wearing <span>Krusty Krab</span>’s official uniform. All these elements contributed to the restaurant being one of Egypt’s most important landmarks visited by presidents, ambassadors, scientists, and artists from all around the world.<br><br>
+    <span>Krusty Krab</span> became famous most for quality. The restaurant only uses the highest quality of meat with standards that cannot be compromised under any circumstances. The calf is fed only high quality food and given the proper care in terms of weather and location. When it comes to the cuisine, the same level of standards is not compromised starting from the fresh spices that add a special flavor, aroma, and taste to the food till the high quality coal that the food is grilled on. The coal is extracted from specific trees like fruit trees that add a delicious scent to the food that has been significant for the main branch of <span>Krusty Krab</span> (El-Qasr El-Einy) for over 40 years.<br><br>
+    In 1985, with the new generation and ideologies, <span>Krusty Krab</span>’s touristic restaurants transformed in order to keep up with the rapid openness the society has witnessed. In 1989, the second branch has been opened, a Five Star Restaurant that quickly became prominent for its luxurious and strategic location in Gameat El Dowal Street in El-Mohandeseen area. The restaurant was the first to introduce the concept of Franchising to the Egyptian market and the Arab World in collaboration with a Saudi Arabian company (<span>Krusty Krab</span> Saudi Arabia). Since that collaboration and the introduction of franchising, <span>Krusty Krab</span>’s international name became (The Eastern <span>Krusty Krab</span>). After the success of the first franchising, another branch was open in Dumiat in 2016.  The restaurant openings, then, proceed with Heliopolis branch in 1994 which overlooks El-Orouba Street in one of the best locations in Cairo. Next, El-Shams club branch came in 1996 to engage with the youth followed by El-Maadi branch in 2004. Another spacious and unique branch opened in Alexandria, the beauty of the Mediterranean in 2003. Moreover, another branch opened in one of Cairo’s most luxurious locations by the Nile bank; located next to the Four Seasons followed by a new branch in 2006 located in New Cairo.   <br><br>
+    Not long after, the North Coast witnessed its first <span>Krusty Krab</span> near gate four in 2006. In 2017, El-Sheikh Zayed City in 6th of October witnessed a luxurious and elegant branch that catered to the people living there.<br><br>
+    We can also never forget the most prominent branch located in front of the pyramids embraced by history and the beauty of the sound and light which opened in 2014 and is truly considered one of the most important landmarks that welcomes foreigners, tourists, and Arabs from all over the world.
+    From one generation to another, the restaurant changed into a big, well-known chain until it became a company working in the field of hotels, restaurants, and companies management in 1998 while in 2009, it has evolved into a touristic group with the name of the Seven Company for touristic projects overlooking projects like (<span>Krusty Krab</span>, Fish and Fisher, Shakra-to-go, Farak).<br><br>
+    In 2000, the catering sector has evolved and became renowned for its high quality, excellent service, and delicious food. Not only that, but it also expanded to include international conferences, company and university events and it became a key competitor in the field of catering as it shone through with the distinction of its name, exceptional quality, and wonderful service. <br><br>
+    Since that day on, the management board overlooked the process of expansion and renovation of the company. This renovation has attracted world talents and experts with professional experience in management as well as cooking. These experts and their experience contributed greatly to expanding the company and ensuring its development in regards to quality, trainings, human resources, and marketing to keep up with the international standards.<br><br>
+    This growth and expansion created a need for food production hence, the company expanded in that field to cater for the needs of the restaurants. The food production process included different processes like cutting, seasoning, packing, and packaging. Every step was taken care of in order for the material to be perfectly summoned and ready for cooking without any sort of intervention from the employees. <br><br>
+    The future looks very bright for <span>Krusty Krab</span> the plan for 2018 includes an expansion by franchising to the Gulf Countries as well as Europe. <br><br>
+    The company is also planning to enter the field of food production for super markets, restaurants, and hotels with its new factory in Egypt by 2020. In addition to that, the plan also includes opening a Human Resources company to cater for the needs of the field of employment in the tourism sector.<br><br>
+    <strong ><span>Krusty Krab</span>, the original taste to the Egyptian cuisine</p>
     </body>
 </html>
+
+
+
