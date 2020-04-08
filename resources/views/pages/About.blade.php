6ebeb6e88b14be4a594b0a52e671c0ddda7143e9
omarsobhy11 <32989189+omarsobhy11@users.noreply.github.com>
2020-04-08 02:07:36 +0200

    first commit

---
+++ resources/views/pages/About.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{config('app.name', 'PizaaApp')}}</title>
+
+    </head>
+    <body>
+        <h1>About</h1>
+        <p>this is the about page made by abdallah mohamed</p>
+    </body>
+</html>
